7749e63f956a24a59c653426351cbfa062c6af44
Josiah Ayres <josiahayres@gmail.com>
2022-07-09 23:44:30 +1200

    Export from correct place

--- src/preset.js
+++ src/preset.js
@@ -1,5 +1,6 @@
 function managerEntries(entry = []) {
   return [...entry, require.resolve("./register")]; //👈 Addon implementation
 }
+export * from "./dist/mantineTheme";
 
 module.exports = { managerEntries };
