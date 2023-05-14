0c34c2d333089176fcdb5cf2fc159aae1df42ccf
Josiah Ayres <josiahayres@gmail.com>
2023-05-14 20:52:11 +1200

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslintPlugin from 'vite-plugin-eslint';
+import react from "@vitejs/plugin-react"; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    eslintPlugin({
-      exclude: ['/virtual:/**', 'node_modules/**'],
-  })],
+  ],
 });
 
