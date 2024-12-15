1182066c0808568e29637c8f6f8b4934512908bb
Josiah Ayres <josiahayres@gmail.com>
2024-12-15 20:30:13 +1300

    Update from latest template

--- src/preview.tsx
+++ src/preview.tsx
@@ -15,11 +15,6 @@ import { MantineProvider } from "@mantine/core";
 import React from "react";
 import { Preview } from "@storybook/react";
 
-/**
- * Note: if you want to use JSX in this file, rename it to `preview.tsx`
- * and update the entry prop in tsup.config.ts to use "src/preview.tsx",
- */
-
 const preview: Preview = {
   decorators: [
     (RenderStory, context) => {
