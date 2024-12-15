55272a8ae42dae955785b6443caa1c76fd3ec67b
Josiah Ayres <josiahayres@gmail.com>
2024-12-15 21:04:40 +1300

    Swap to npm

--- src/preview.tsx
+++ src/preview.tsx
@@ -1,18 +1,6 @@
-/**
- * A decorator is a way to wrap a story in extra “rendering” functionality. Many addons define decorators
- * in order to augment stories:
- * - with extra rendering
- * - gather details about how a story is rendered
- *
- * When writing stories, decorators are typically used to wrap stories with extra markup or context mocking.
- *
- * https://storybook.js.org/docs/react/writing-stories/decorators
- */
-
+import React from "react";
 import { PARAM_KEYS } from "./constants";
 import { MantineProvider } from "@mantine/core";
-
-import React from "react";
 
 const preview: any = {
   decorators: [
