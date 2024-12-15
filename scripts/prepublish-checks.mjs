9c38be40dd5fbb191a7da813662b7c008933502c
Josiah Ayres <josiahayres@gmail.com>
2024-12-15 14:59:40 +1300

    Use the new template

--- scripts/prepublish-checks.mjs
+++ scripts/prepublish-checks.mjs
@@ -2,13 +2,14 @@
 
 import boxen from "boxen";
 import dedent from "dedent";
+import { readFile } from "node:fs/promises";
+import { globalPackages as globalManagerPackages } from "storybook/internal/manager/globals";
+import { globalPackages as globalPreviewPackages } from "storybook/internal/preview/globals";
 
-const loadJSON = (path) =>
-  JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
+const packageJson = await readFile("./package.json", "utf8").then(JSON.parse);
 
-const a = loadJSON("../package.json");
-const name = a.name;
-const displayName = a.storybook.displayName;
+const name = packageJson.name;
+const displayName = packageJson.storybook.displayName;
 
 let exitCode = 0;
 $.verbose = false;
@@ -55,5 +56,30 @@ if ((await $`cat README.md | grep -E ${readmeTestStrings}`.exitCode) == 0) {
 
   exitCode = 1;
 }
+
+/**
+ * Check that globalized packages are not incorrectly listed as peer dependencies
+ */
+const peerDependencies = Object.keys(packageJson.peerDependencies || {});
+const globalPackages = [...globalManagerPackages, ...globalPreviewPackages];
+peerDependencies.forEach((dependency) => {
+  if (globalPackages.includes(dependency)) {
+    console.error(
+      boxen(
+        dedent`
+          ${chalk.red.bold("Unnecessary peer dependency")}
+  
+          ${chalk.red(dedent`You have a peer dependency on ${chalk.bold(dependency)} which is most likely unnecessary
+          as that is provided by Storybook directly.
+          Check the "bundling" section in README.md for more information.
+          If you are absolutely sure you are doing it correct, you should remove this check from scripts/prepublish-checks.js.`)}
+        `,
+        { padding: 1, borderColor: "red" }
+      )
+    );
+
+    exitCode = 1;
+  }
+});
 
 process.exit(exitCode);
